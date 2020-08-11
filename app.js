440a0f72a963cf13fa0a9a26cf7223c6ff9b2765
Brian Levin <bml201095@gmail.com>
2020-08-11 19:18:37 -0400

    added an if staement  with methods showing if the player won the game

--- app.js
+++ app.js
@@ -35,9 +35,23 @@ guessBtn.addEventListener('click', function(){
      // Validate
   if(isNaN(guess) || guess < min || guess > max){
     setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+
   }
+   // Check if won
+   if(guess === winningNum){
+    // Disable input
+    guessInput.disabled = true;
+    // Change border color
+    guessInput.style.borderColor = 'green';
+    // Set message
+    setMessage(`${winningNum} is correct, YOU WIN!`, 'green');
+
+  } else {
+
+  }
+});
     
-})
+
 
 // Set message
 function setMessage(msg, color){
